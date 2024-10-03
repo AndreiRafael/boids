06d7b44c19ee596b86a64f4dd5ad61c85991f1d2
Andrei Rafael Brongel <abrongel@gmail.com>
2024-10-03 16:29:57 -0300

    fixes build windows

--- src/main.c
+++ src/main.c
@@ -27,7 +27,7 @@ int main(int argc, char** argv) {
     SDL_Init(SDL_INIT_VIDEO);
 
     SDL_Window* window = SDL_CreateWindow(
-        "pngtuber",
+        "boids",
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
         WINDOW_W, WINDOW_H,
         SDL_WINDOW_OPENGL
